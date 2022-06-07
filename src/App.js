e3bb99ddcd633a5fd6f6b9e5734da2b7ed962f1e
박정환 <19park@wholedoc.net>
2022-06-08 07:37:53 +0900

    styled-components - theme provider

--- src/App.js
+++ src/App.js
@@ -1,45 +1,23 @@
-import styled, {keyframes} from 'styled-components'
+import styled from 'styled-components'
 
 const Wrapper = styled.div`
   display: flex;
-`;
-const rotateAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0;
-  }
-  50% {
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(360deg);
-    border-radius: 0;
-  }
-`;
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-const Box = styled.div`
-  background-color: tomato;
-  width: 200px;
-  height: 200px;
-  display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-  animation: ${rotateAnimation} 1s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 60px;
-    }
-  }
+  background-color: ${props => props.theme.backgroundColor}
+`;
+const Title = styled.h1`
+  color: ${props => props.theme.textColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>★</Emoji>
-      </Box>
+      <Title>
+        Hello
+      </Title>
     </Wrapper>
   );
 }
