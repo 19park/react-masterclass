3b68c1fe5d21c0c89b90d81e9a6bb671c59aa421
박정환 <19park@wholedoc.net>
2022-06-08 07:08:40 +0900

    styled-components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+
+const Father = styled.div`
+  display: flex;
+`;
+const Box = styled.div`
+  background-color: ${props => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+const Circle = styled(Box)`
+  background-color: ${props => props.bgColor};
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+`;
+const Btn = styled.button`
+  color: #fff;
+  background-color: green;
+  border-radius: 15px;
+  padding: 2px 5px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Father>
+      <Box bgColor="teal"/>
+      <Circle bgColor="tomato"/>
+
+      <Btn>Login</Btn>
+      <Btn as="a" href="#">Login</Btn>
+    </Father>
   );
 }
 
