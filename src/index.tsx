40efd74413cbe08325c348d1875b64582e143aab
박정환 <19park@wholedoc.net>
2022-06-08 13:03:15 +0900

    event type, define type

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
+import { lightTheme } from './theme';
 
 const el = document.getElementById('root') as HTMLDivElement | DocumentFragment;
 const root = ReactDOM.createRoot(el);
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={lightTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
