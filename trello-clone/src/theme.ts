7e0d29a69e9b8b8a345f4318d0ab662347812800
박정환 <19park@wholedoc.net>
2022-06-16 21:53:55 +0900

    init trello-clone #2

---
+++ trello-clone/src/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme:DefaultTheme = {
+    bgColor: "#2f3640",
+    boxColor: "#fff",
+    textColor: "#f5f6fa",
+    accentColor: "#4cd137",
+};
+
+export const lightTheme:DefaultTheme = {
+    bgColor: "#ddd",
+    boxColor: "#2f3640",
+    textColor: "#eee",
+    accentColor: "#FF005A",
+};
